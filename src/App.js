033ae9479fa6ce7e4c9230f6653fa84ec5ef810b
deepak <deepamdeebu@gmail.com>
2023-12-10 20:15:14 +0530

    Devtool : Add in react query devtools and abd check with some config

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Planets from './components/Planets';
 import People from './components/People';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
@@ -11,15 +12,18 @@ function App() {
   let [page, setPage] = useState('planets');
 
   return (
-    <div className="App">
-    <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        <QueryClientProvider client={queryClient}>
-          { page === 'planets' ? <Planets /> : <People /> }
-        </QueryClientProvider>
+    <>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          <QueryClientProvider client={queryClient}>
+            { page === 'planets' ? <Planets /> : <People /> }
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
