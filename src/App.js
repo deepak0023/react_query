5cce2de203a8b0265e9234f3a123f17d63ca217b
deepak <deepamdeebu@gmail.com>
2023-12-10 19:30:57 +0530

    React Query : Fetch Swapi API using react query and also add some styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Planets from './components/Planets';
 import People from './components/People';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
 
@@ -12,7 +15,9 @@ function App() {
     <h1>Star Wars Info</h1>
       <Navbar setPage={setPage} />
       <div className="content">
-        { page === 'planets' ? <Planets /> : <People /> }
+        <QueryClientProvider client={queryClient}>
+          { page === 'planets' ? <Planets /> : <People /> }
+        </QueryClientProvider>
       </div>
     </div>
   );
