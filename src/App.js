45ee8c9968fe34177a58dc8837ea07e44b8c8f32
deepak <deepamdeebu@gmail.com>
2023-12-10 18:49:51 +0530

    Components : Added some base components to start working on react query

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import Planets from './components/Planets';
+import People from './components/People';
 
 function App() {
+
+  let [page, setPage] = useState('planets');
+
   return (
     <div className="App">
-      <div className="contents">
+    <h1>Star Wars Info</h1>
+      <Navbar setPage={setPage} />
+      <div className="content">
+        { page === 'planets' ? <Planets /> : <People /> }
       </div>
     </div>
   );
